fc277fff5da6dd175e94b560af75b5c094336349
joonsik <ryan0@kakao.com>
2022-04-22 22:46:20 +0900

    spring initializer

---
+++ src/main/java/com/smrp/smartmedicinealarm/SmartMedicineAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.smrp.smartmedicinealarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMedicineAlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMedicineAlarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smrp/smartmedicinealarm/SmartMedicineAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smrp.smartmedicinealarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMedicineAlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
