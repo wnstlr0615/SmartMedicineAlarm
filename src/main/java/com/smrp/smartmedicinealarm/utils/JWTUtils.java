f26e670faf5e6bf0d858f5e1f1da7c9a4747c068
joonsik <ryan0@kakao.com>
2022-05-04 15:51:29 +0900

    Add JWTUtils 생성 JWT유틸 생성 및 테스트 코드 추가

---
+++ src/main/java/com/smrp/smartmedicinealarm/utils/JWTUtils.java
@@ -0,0 +1,63 @@
+package com.smrp.smartmedicinealarm.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.smrp.smartmedicinealarm.error.code.LoginErrorCode;
+import com.smrp.smartmedicinealarm.error.exception.LoginException;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.Assert;
+
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Slf4j
+@UtilityClass
+public class JWTUtils {
+    @Value("${jwt.accessExpiredTime}")
+    private Duration accessTokenExpiredTime;
+
+    @Value("${jwt.refreshExpiredTime}")
+    private Duration refreshTokenExpiredTime;
+
+    @Value("${jwt.key}")
+    private String key;
+
+    public static String createAccessToken(String username){
+        return createToken(username, accessTokenExpiredTime, Algorithm.HMAC512(key));
+    }
+
+    public static String createRefreshToken(String username){
+        return createToken(username, refreshTokenExpiredTime, Algorithm.HMAC512(key));
+    }
+
+    public static String createToken(String username, Duration expiredTime, Algorithm key){
+        Assert.hasText(username, "올 바르지 않은 username 입니다.");
+        LocalDateTime expiredDate = LocalDateTime.now().plusSeconds(expiredTime.getSeconds());
+        Timestamp expirationTime = Timestamp.valueOf(expiredDate);
+
+        return JWT.create()
+                .withExpiresAt(expirationTime)
+                .withSubject(username)
+                .sign(key);
+    }
+
+    public static String verifyToken(String token){
+        return verifyToken(token, key);
+    }
+
+    public static String verifyToken(String token, String key){
+        Assert.hasText(token, "token 이 올 바르지 않습니다. ");
+        DecodedJWT decodedJWT;
+        try {
+            decodedJWT = JWT.require(Algorithm.HMAC512(key)).build().verify(token);
+        } catch (JWTVerificationException | IllegalArgumentException  e) {
+            throw new LoginException(LoginErrorCode.TOKEN_IS_EXPIRED_OR_WRONG);
+        }
+        return decodedJWT.getSubject();
+    }
+}
---
+++ src/test/java/com/smrp/smartmedicinealarm/utils/JWTUtilsTest.java
@@ -0,0 +1,82 @@
+package com.smrp.smartmedicinealarm.utils;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.smrp.smartmedicinealarm.error.code.LoginErrorCode;
+import com.smrp.smartmedicinealarm.error.exception.LoginException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+class JWTUtilsTest {
+    final String key = "key";
+    @Test
+    @DisplayName("[성공] JWT 토큰 생성")
+    public void successCreateToken (){
+        //given
+        Duration accessExpiredTime = Duration.ofHours(1);
+        String username = "joon@naver.com";
+
+        //when
+        String token = JWTUtils.createToken(username, accessExpiredTime, Algorithm.HMAC512(key));
+
+        // then
+        assertThat(token).isNotBlank();
+    }
+
+    @Test
+    @DisplayName("[실패] JWT 토큰 생성 - username 이 공백이거나 null인 경우")
+    public void failCreateToken (){
+        //given
+
+        Duration accessExpiredTime = Duration.ofHours(1);
+        String username = "";
+
+        //when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> JWTUtils.createToken(username, accessExpiredTime, Algorithm.HMAC512(key))
+        );
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("올 바르지 않은 username 입니다.");
+    }
+
+    @Test
+    @DisplayName("[성공] JWT 토큰 생성 및 검증 ")
+    public void successTokenVerification (){
+        //given
+        Duration accessExpiredTime = Duration.ofHours(1);
+        String username = "joon@naver.com";
+        String token = JWTUtils.createToken(username, accessExpiredTime, Algorithm.HMAC512(key));
+
+        //when //then
+        assertThat(JWTUtils.verifyToken(token, key)).isEqualTo(username);
+
+    }
+
+    @Test
+    @DisplayName("[실패] JWT 토큰 생성 및 검증 - 토큰이 만료한 경우")
+    public void failTokenVerification (){
+        //given
+        LoginErrorCode errorCode = LoginErrorCode.TOKEN_IS_EXPIRED_OR_WRONG;
+        Duration accessExpiredTime = Duration.ofSeconds(-1);
+        String username = "joon@naver.com";
+        String token = JWTUtils.createToken(username, accessExpiredTime, Algorithm.HMAC512(key));
+
+        //when
+        LoginException loginException = assertThrows(LoginException.class,
+                () -> JWTUtils.verifyToken(token, key)
+        );
+
+        //then
+        assertThat(loginException.getErrorCode()).isEqualTo(errorCode);
+    }
+}
