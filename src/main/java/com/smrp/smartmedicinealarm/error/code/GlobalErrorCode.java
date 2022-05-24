73616575476edf2c802e473c314869df4b181d26
joonsik <ryan0@kakao.com>
2022-05-24 12:00:46 +0900

    Add Exception Handler 추가 HttpRequestMethodNotSupportedException.class, HttpMediaTypeNotSupportedException.class, IllegalStateException.class, IllegalArgumentException.class, ConstraintViolationException.class, HttpMessageNotReadableException.class, MethodArgumentNotValidException.class, Exception.class, RuntimeException.class

--- src/main/java/com/smrp/smartmedicinealarm/error/code/GlobalErrorCode.java
+++ src/main/java/com/smrp/smartmedicinealarm/error/code/GlobalErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum GlobalErrorCode implements ErrorCode{
     INTERNAL_SERVER_ERROR("서버에 문제가 발생하였습니다.", 500),
     LOGIN_FAIL("로그인에 실패하였습니다.", 400),
-    UNAUTHORIZED_ERROR("접근 권한이 없습니다.", 403)
+    UNAUTHORIZED_ERROR("접근 권한이 없습니다.", 403),
+    BAD_REQUEST(" 잘못된 요청 입니다.", 404),
+    UNSUPPORTED_MEDIA_TYPE("지원하지 않는 미디어 타입 입니다..", 415),
+    METHOD_NOT_ALLOWED("허용 되지 않은 메소드 요청입니다.", 405)
     ;
     private final String description;
     private final int status;
--- src/main/java/com/smrp/smartmedicinealarm/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/smrp/smartmedicinealarm/error/handler/GlobalExceptionHandler.java
@@ -1,30 +1,101 @@
 package com.smrp.smartmedicinealarm.error.handler;
 
+import com.smrp.smartmedicinealarm.error.code.ErrorCode;
+import com.smrp.smartmedicinealarm.error.code.GlobalErrorCode;
 import com.smrp.smartmedicinealarm.error.exception.UserException;
 import com.smrp.smartmedicinealarm.error.response.ErrorResponse;
+import com.smrp.smartmedicinealarm.error.response.FieldErrorDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserException.class)
-    public ResponseEntity<?> userExceptionHandler(UserException e, HttpServletRequest request){
+    private ResponseEntity<?> userExceptionHandler(UserException e, HttpServletRequest request){
         printLog(e, request);
-        return ResponseEntity.status(
-                e.getErrorCode().getStatus()
-        ).body(
+        return createErrorResponse(e);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e, HttpServletRequest request){
+        log.debug("MethodArgumentNotValidException", e);
+        return createErrorResponse(GlobalErrorCode.BAD_REQUEST, e.getAllErrors());
+    }
+    @ExceptionHandler({
+            IllegalStateException.class,
+            IllegalArgumentException.class,
+            ConstraintViolationException.class,
+            HttpMessageNotReadableException.class
+    })
+    private ResponseEntity<?> BadRequestExceptionHandler(Exception e, HttpServletRequest request) {
+        printLog(e, request);
+        return createErrorResponse(GlobalErrorCode.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<?> httpMediaTypeNotSupportedExceptionHandler(Exception e, HttpServletRequest request){
+        printLog(e, request);
+        return createErrorResponse(GlobalErrorCode.UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<?> httpRequestMethodNotSupportedExceptionHandler(Exception e, HttpServletRequest request){
+        printLog(e, request);
+        return createErrorResponse(GlobalErrorCode.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public ResponseEntity<?> exceptionHandler(Exception e, HttpServletRequest request){
+        printLog(e, request);
+        return createErrorResponse(GlobalErrorCode.INTERNAL_SERVER_ERROR);
+    }
+    private ResponseEntity<?> createErrorResponse(ErrorCode errorCode, List<ObjectError> allErrors) {
+        List<FieldErrorDto> fieldErrorDtos = getFieldErrorDtos(allErrors);
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(
+                        ErrorResponse.CreateErrorResponse(errorCode, fieldErrorDtos)
+                );
+    }
+
+    private List<FieldErrorDto> getFieldErrorDtos(List<ObjectError> allErrors) {
+        return allErrors.stream().filter(objectError -> objectError instanceof FieldError)
+                .map(objectError -> (FieldError) objectError).map(FieldErrorDto::fromFieldError).collect(Collectors.toList());
+    }
+
+    private ResponseEntity<ErrorResponse> createErrorResponse(UserException e) {
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(
                 ErrorResponse.CreateErrorResponse(e.getErrorCode())
         );
     }
+    private ResponseEntity<?> createErrorResponse(ErrorCode errorCode) {
+        return createErrorResponse(errorCode, errorCode.getDescription());
+    }
+    private ResponseEntity<?> createErrorResponse(ErrorCode errorCode, String errorMessage) {
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(
+                        ErrorResponse.CreateErrorResponse(errorCode, errorMessage)
+                );
+    }
 
-    private void printLog(UserException e, HttpServletRequest request) {
+
+    private void printLog(Exception e, HttpServletRequest request) {
         String uuid = (String) request.getAttribute("UUID");
         log.error("[{}] requestURL : {}",uuid, request.getRequestURL());
-        log.error("[{}] errorCode : {}, errorMessage : {}",uuid, e.getErrorCode(), e.getErrorMessage());
+        log.error("[{}] errorMessage : {}",uuid, e.getMessage());
+        log.error("",e);
     }
 }
