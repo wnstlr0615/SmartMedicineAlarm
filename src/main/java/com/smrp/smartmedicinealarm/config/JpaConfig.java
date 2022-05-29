79c0783c5eb4bb5d6591008c988d49804917cbf9
joonsik <ryan0@kakao.com>
2022-05-29 22:54:07 +0900

    Add BaseEntity 구현 만든 사람과 수정한 사람이 저장됨

--- src/main/java/com/smrp/smartmedicinealarm/config/JpaConfig.java
+++ src/main/java/com/smrp/smartmedicinealarm/config/JpaConfig.java
@@ -1,11 +1,15 @@
 package com.smrp.smartmedicinealarm.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.smrp.smartmedicinealarm.entity.account.Account;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.persistence.EntityManager;
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
@@ -13,5 +17,13 @@ public class JpaConfig {
     @Bean
     JPAQueryFactory jpaQueryFactory(EntityManager em){
         return new JPAQueryFactory(em);
+    }
+
+    @Bean
+    AuditorAware<String> auditorProvider(){
+        return () -> {
+            Account account = (Account) SecurityContextHolder.getContext().getAuthentication();
+            return Optional.of(account.getAccountId().toString());
+        };
     }
 }
