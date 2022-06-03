8e46ecdb81077982d760ac9b80fea719d133d1ac
joonsik <ryan0@kakao.com>
2022-06-03 12:33:15 +0900

    Add ResponseResult 생성

---
+++ src/main/java/com/smrp/smartmedicinealarm/dto/ResponseResult.java
@@ -0,0 +1,27 @@
+package com.smrp.smartmedicinealarm.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseResult {
+    private boolean result;
+    private String message;
+    private Object data;
+
+    public static ResponseResult success(String message, Object data) {
+        return ResponseResult.builder()
+                .result(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static ResponseResult success(String message) {
+        return success(message, null);
+    }
+}
